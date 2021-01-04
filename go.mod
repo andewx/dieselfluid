d76bf65a50e083b771b2271d47c74dfcd42a60ca
andewx <brian.michael.anderson@gmail.com>
2021-01-04 15:12:44 +0900

    Update SPH package file name particlesystem.go -> fluid.go-Added Stubs

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module dslfluid.com/dsl
 go 1.15
 
 require (
+	4d63.com/gochecknoglobals v0.0.0-20201008074935-acfc0b28355a // indirect
 	github.com/andewx/dieselfluid/math/math64 v0.0.0-20210102013021-9c8bad47080d
 	github.com/andewx/dieselfluid/sampler/voxel v0.0.0-20210103184235-121b09a298e7
+	honnef.co/go/tools v0.1.0 // indirect
 )
---
+++ model/sph/fluid.go
@@ -0,0 +1,107 @@
+package sph
+
+//Provides particle Interface - Implements basic particles and particle system
+//for reuse with application specific SPH methods
+
+import (
+	V "dslfluid.com/dsl/math/math64" //Diesel Vector Library - Simple Vec
+	F "dslfluid.com/dsl/model/field"
+)
+
+//SPHSystem represents the core SPH iteration
+type SPHSystem interface {
+	GetPos() []V.Vec               //Getter
+	GetVel() []V.Vec               //Getter
+	GetDens() []float64            //Getter
+	GetForce() []V.Vec             //Getter
+	GetPress() []V.Vec             //Getter
+	TimeStep() float64             //Getter
+	UpdateTime() float64           //Update Time Delta
+	Length() int                   //Num Particles
+	Run() error                    //Runs SPH Computation Loop
+	Run_Threaded(t chan int) error //Runs SPH computation loop as thread
+}
+
+//SPH Standard SPH Particle System - Implements SPHSystem Interface
+type SPHCore struct {
+	Pos    []V.Vec    //Positions
+	Dens   []float64  //Densities
+	Vels   []V.Vec    //Velocities
+	Fs     []V.Vec    //Forces
+	Ps     []float64  //Pressures
+	Time   float64    //Time Step
+	MaxVel float64    //Max Vel - Courant Condition
+	Field  F.SPHField //Gradient Differential Methods
+}
+
+//-----------Implements SPHCore -----------------------------
+func (p SPHCore) GetPos() []V.Vec {
+	return p.Pos
+}
+func (p SPHCore) GetVel() []V.Vec {
+	return p.Vels
+}
+func (p SPHCore) GetDens() []float64 {
+	return p.Dens
+}
+func (p SPHCore) GetForce() []V.Vec {
+	return p.Fs
+}
+func (p SPHCore) GetPress() []float64 {
+	return p.Ps
+}
+func (p SPHCore) TimeStep() float64 {
+	return p.Time
+}
+func (p SPHCore) UpdateTime() float64 {
+	K := 0.4 * p.Field.Part.KernelRad() / p.Field.Part.Sound()
+	A := p.Field.Part.KernelRad() / p.MaxVel
+
+	if A < K {
+		p.Time = A
+		return A
+	}
+	p.Time = K
+	return K
+}
+
+//-----------SPH Core Private (From Interface) Methods --------------------//
+
+//AccumulatePressure -- Accumulates Particle Pressure Forces
+func (p SPHCore) AccumulatePressure() int {
+	return 0
+}
+
+//Accumulate_NonPressure -- accumulates non pressure forces (Viscosity)
+func (p SPHCore) Accumulate_NonPressure() int {
+	return 0
+}
+
+//External adds external forces -- (Gravity - Wind Resist Etc)
+func (p SPHCore) External(force V.Vec) {
+
+}
+
+//ComputeDensity calculates particle density -- Maps Particle Density to Pressure
+func (p SPHCore) ComputeDensity() {
+
+}
+
+//Collide handles particle collision
+func (p SPHCore) Collide() {
+
+}
+
+//Update updates all particle positions -- non-blocking mutex locked
+func (p SPHCore) Update() {
+
+}
+
+//---------SPHCore Run Methods------------------------//
+func (p SPHCore) Run() error {
+	return nil
+}
+
+func (p SPHCore) Run_Threaded(t chan int) error {
+	return nil
+}
